883fccc12e18c24a76685c04f097ad122e4d99bb
Anna M Attehed <anna.attehed@medieinstitutet.se>
2022-09-28 15:29:52 +0200

    Update search.php

---
+++ themes/theme_twelve/search.php
@@ -0,0 +1,21 @@
+<?php
+
+get_header();
+
+if (have_posts()) {
+?>
+    <h3>Search results:</h3>
+
+<?php
+    while (have_posts()) {
+
+        // Här kan ex. templatepart läggas in för att visa resultat.
+
+    }
+} else {
+    echo '<p>Sorry no results where found</p>';
+}
+
+
+
+get_footer();
