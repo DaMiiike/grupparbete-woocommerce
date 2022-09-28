9e1b06344c284136be2f4e8202523866781c2292
Anna M Attehed <anna.attehed@medieinstitutet.se>
2022-09-28 09:42:21 +0200

    enable use of custom logo in theme

--- themes/anotheremptytheme/functions.php
+++ themes/anotheremptytheme/functions.php
@@ -1,8 +1,14 @@
 <?php
 
+// enable stylesheets 
+
 function theme_twelve_enqueue()
 {
   wp_enqueue_style('style', get_stylesheet_uri());
 }
 
 add_action('wp_enqueue_scripts', 'theme_twelve_enqueue');
+
+// enable use of custom logo in theme 
+
+add_theme_support( 'custom-logo' );
