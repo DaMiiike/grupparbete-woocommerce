31b93c516a7246ce458386efd07f99e567c7c0c2
DaMiiike <michael.tariao@medieinstitutet.se>
2022-09-29 11:06:09 +0200

    commiting, need to get latest from main

---
+++ themes/anotheremptytheme/front-page.php
@@ -0,0 +1,9 @@
+<?php
+get_header();
+?>
+front-page.php 
+
+
+<?php
+get_footer();
+?>
--- themes/anotheremptytheme/functions.php
+++ themes/anotheremptytheme/functions.php
@@ -5,3 +5,8 @@ function anotheremptytheme_enqueue()
 {
   wp_enqueue_style('style', get_stylesheet_uri());
 }
+
+add_action( 'acf/init', 'my_acf_init_block_types' );
+function my_acf_init_block_types() {
+    register_block_type( __DIR__ . '/blocks/testimonial' );
+}
