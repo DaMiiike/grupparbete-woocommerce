baa9caf6b1dd58c69f65534a2f448bc4e436053a
Anna M Attehed <anna.attehed@medieinstitutet.se>
2022-10-04 13:30:11 +0200

    Payment methods added

--- themes/theme_twelve/front-page.php
+++ themes/theme_twelve/front-page.php
@@ -6,9 +6,11 @@ get_header();
 front page
 <hr>
 
-<?php the_content();?>
-
-<?php echo wp_get_attachment_image('thumbnail' ); ?> 
+<?php 
+$image = get_field('hero_image');
+if( !empty( $image ) ): ?>
+    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+<?php endif; ?>
 
 <?php
 get_footer();
--- themes/theme_twelve/functions.php
+++ themes/theme_twelve/functions.php
@@ -98,26 +98,8 @@ function mytheme_add_woocommerce_support()
 add_action('after_setup_theme', 'mytheme_add_woocommerce_support');
 
 
-// Register new post type - Stores 
+// Register new custom post type - Stores 
 
-function create_posttype()
-{
-  register_post_type(
-    'stores',
-    // CPT Options
-    array(
-      'labels' => array(
-        'name' => __('Stores'),
-        'singular_name' => __('Store')
-      ),
-      'public' => true,
-      'has_archive' => false,
-      'rewrite' => array('slug' => 'stores'),
-    )
-  );
-}
-// Hooking up our function to theme setup
-add_action('init', 'create_posttype');
  
 // Register Custom Post Type
 function custom_post_type() {
@@ -125,25 +107,56 @@ function custom_post_type() {
 	$labels = array(
 		'name'                  => _x( 'Stores', 'Post Type General Name', 'text_domain' ),
 		'singular_name'         => _x( 'Store', 'Post Type Singular Name', 'text_domain' ),
-  );
+		'menu_name'             => __( 'Stores', 'text_domain' ),
+		'name_admin_bar'        => __( 'Stores', 'text_domain' ),
+		'archives'              => __( 'Item Archives', 'text_domain' ),
+		'attributes'            => __( 'Item Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Items', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Item', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Item', 'text_domain' ),
+		'edit_item'             => __( 'Edit Item', 'text_domain' ),
+		'update_item'           => __( 'Update Item', 'text_domain' ),
+		'view_item'             => __( 'View Item', 'text_domain' ),
+		'view_items'            => __( 'View Items', 'text_domain' ),
+		'search_items'          => __( 'Search Item', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$rewrite = array(
+		'slug'                  => 'stores',
+		'with_front'            => true,
+		'pages'                 => true,
+		'feeds'                 => true,
+	);
 	$args = array(
 		'label'                 => __( 'Store', 'text_domain' ),
 		'description'           => __( 'Post Type Description', 'text_domain' ),
 		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'thumbnail', 'page-attributes' ),
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields', 'page-attributes' ),
 		'taxonomies'            => array( 'category', 'post_tag' ),
 		'hierarchical'          => false,
 		'public'                => true,
 		'show_ui'               => true,
 		'show_in_menu'          => true,
 		'menu_position'         => 5,
-		'menu_icon'             => 'dashicons-cart',
 		'show_in_admin_bar'     => true,
 		'show_in_nav_menus'     => true,
 		'can_export'            => true,
 		'has_archive'           => true,
 		'exclude_from_search'   => false,
 		'publicly_queryable'    => true,
+		'rewrite'               => $rewrite,
 		'capability_type'       => 'page',
 	);
 	register_post_type( 'stores', $args );
