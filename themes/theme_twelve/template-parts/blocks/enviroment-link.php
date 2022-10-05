332d350006f977a41bde416324d3bd617339d85d
Anna M Attehed <anna.attehed@medieinstitutet.se>
2022-10-05 10:53:24 +0200

    Changes in enviroment link block

--- themes/theme_twelve/template-parts/blocks/enviroment-link.php
+++ themes/theme_twelve/template-parts/blocks/enviroment-link.php
@@ -1,16 +1,21 @@
 <div class="enviroment-container">
 
-<?php the_field('enviroment_heading'); ?> 
-<?php the_field('enviroment_text_area'); ?>
+    <h1 class="enviroment-heading">
+        <?php the_field('enviroment_heading'); ?>
+    </h1>
 
-<?php 
+    <article class="enviroment-text">
+        <?php the_field('enviroment_text_area'); ?>
+    </article>
 
-$link = get_field('enviroment-link');
-if( $link ): ?>
-    <a class="button" href="<?php echo esc_url( $link ); ?>">Continue Reading</a>
-<?php endif; ?>
+
+    <div class="enviroment-link">
+        <?php
+        $link = get_field('enviroment_link');
+        if ($link) : ?>
+            <a class="button" href="<?php echo esc_url($link); ?>">Continue Reading</a>
+        <?php endif; ?>
+    </div>
+
 
 </div>
-
-
-
